1ebb9a10583ce6d7e8cc9ebf2b75c1f5b078c8ef
Shako Kinyamba <shakokinyamba201@gmail.com>
2022-11-08 11:23:32 +0200

    Feat: functions mapState and mapDispatch

--- src/components/Phone.jsx
+++ src/components/Phone.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import phone  from "/src/assets/tel.jpeg"
-const Phone = () => {
+import phone from "/src/assets/tel.jpeg"
+import { connect } from 'react-redux';
+import { bay_phone } from '../redux/phone/actions';
+
+const Phone = (props) => {
+    console.log(props);
     return (
         <div className='container'>
             <img src={phone} alt="" srcset="" />
-            <p>Disponible: <span className="counter"></span></p>
-            <button className='bay_phone'>Acheter</button>
+            <p>Disponible: <span id="counter">{props.phones}</span></p>
+            <button className='bay_phone' onClick={() => props.bay_phone()}>Acheter</button>
         </div>
     );
 };
+// la fonction pour accede au state du store
+const mapStateToprops = (state) => {
+    return {
+        phones: state.phones
+    }
+}
 
-export default Phone;
+// la fonction pour dispatch l'action
+const mapDispatchprops = (dispatch) => {
+    return {
+        bay_phone: () => dispatch(bay_phone())
+    }
+}
+
+export default connect(mapStateToprops, mapDispatchprops)(Phone);
